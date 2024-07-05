196e0a61a7618388ed26ef7c64ade180456772da
Daniel Carrillo <danielcarrillo216@gmail.com>
2024-07-04 23:36:03 -0600

    titulos y parrafos

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML='juego del numero secreto';
+
+let parrafo=document.querySelector('p');
+parrafo.innerHTML='escoje un numero';
+
+function intentodeUsuario(){
+    
+}
