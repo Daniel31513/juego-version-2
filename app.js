5365dd10585100731d73c80dd3bdf93c43940d5e
Daniel Carrillo <danielcarrillo216@gmail.com>
2024-07-05 20:20:04 -0600

    numero randoom y boton

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML='juego del numero secreto';
+let numeroSecreto = generarNumeroSecreto();
 
-let parrafo=document.querySelector('p');
-parrafo.innerHTML='escoje un numero';
 
-function intentodeUsuario(){
-    
+function asignarTextoElemento(elemento,texto){
+let elementohtml=document.querySelector(elemento)
+elementohtml.innerHTML=texto;
+
 }
+function verificarIntento(){
+    let numeroDeUsuario=parseInt(document.getElementById('valorUsuario').value);
+
+    console.log(typeof(numeroDeUsuario));
+    console.log(numeroDeUsuario);
+    console.log(numeroSecreto);
+    console.log(numeroDeUsuario==numeroSecreto);
+    return;
+
+}
+
+function generarNumeroSecreto(){
+    return Math.floor(Math.random()*10)+1;
+}
+
+asignarTextoElemento('h1','Juego del numero secreto!');
+asignarTextoElemento('p','Indica un numero del 1 al 10');
