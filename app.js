bd5e19b9d07d530e62453c9f2e1e204493f142da
Daniel Carrillo <danielcarrillo216@gmail.com>
2024-07-07 10:51:08 -0600

    boton de reinicio

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-let numeroSecreto = generarNumeroSecreto();
-let intentos=1;
+let numeroSecreto = 0;
+let intentos=0;
 
 console.log(numeroSecreto);
+
 function asignarTextoElemento(elemento,texto){
 let elementohtml=document.querySelector(elemento)
 elementohtml.innerHTML=texto;
@@ -16,9 +17,11 @@ function verificarIntento(){
 
     if (numeroDeUsuario===numeroSecreto){
         asignarTextoElemento('p',`Acertaste el numero en ${intentos} ${(intentos==1)?'vez':'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+   
     }
     else{
-
+//el usuario no acepto
         if(numeroSecreto>numeroDeUsuario){
             asignarTextoElemento('p','el numero secreto es mayor');
         }
@@ -26,6 +29,7 @@ function verificarIntento(){
             asignarTextoElemento('p','el numero secreto es menor');
         }
         intentos++;
+        limpiarCaja();
 
     
 }
@@ -33,9 +37,34 @@ function verificarIntento(){
 return;
     }
 
+    function limpiarCaja(){
+    document.querySelector('#valorUsuario').value='';//# siginifica que se requiere por id
+    //.value='' la vuelve vacio
+
+    }
+
 function generarNumeroSecreto(){
     return Math.floor(Math.random()*10)+1;
 }
 
-asignarTextoElemento('h1','Juego del numero secreto!');
-asignarTextoElemento('p','Indica un numero del 1 al 10');
+function condicionesIniciales(){
+    asignarTextoElemento('h1','Juego del numero secreto!');
+    asignarTextoElemento('p','Indica un numero del 1 al 10');
+    numeroSecreto=generarNumeroSecreto();
+    intentos=1;
+}
+function reiniciarJuego(){
+    //limpiar la caja
+    limpiarCaja();
+    //indicar intervalo de numeros
+    //generar el numero aleatorio
+    //inicializar el numero de intentos
+    condicionesIniciales();
+    //deshabilita el boton reinicio
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+
+
+}
+
+
+condicionesIniciales();
